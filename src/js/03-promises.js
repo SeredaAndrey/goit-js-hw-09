583b4a467511638473086f7eb9f46f9e10fa976a
Andrii Sereda <a.a.sereda@ukr.net>
2022-10-21 10:30:01 +0300

    ready third part

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,5 @@
+import Notiflix from 'notiflix';
+
 const refs = {
   form: document.querySelector('.form'),
   delay: document.querySelector('[name="delay"]'),
@@ -15,36 +17,43 @@ refs.form.addEventListener('submit', evt => {
   evt.preventDefault();
   console.log('start promise generator');
   createLoopPromise(promiseVar.delay, promiseVar.amount, promiseVar.step);
-  // evt.currentTarget.reset();
-  // console.log(promiseVar);
 });
+
 refs.delay.addEventListener('input', evt => {
-  // console.log(evt.target.value);
   promiseVar.delay = evt.target.value;
 });
 refs.step.addEventListener('input', evt => {
-  // console.log(evt.target.value);
   promiseVar.step = evt.target.value;
 });
 refs.amount.addEventListener('input', evt => {
-  // console.log(evt.target.value);
   promiseVar.amount = evt.target.value;
 });
 
 function createLoopPromise(startDelay, position, delay) {
   let delayPromise = Number(startDelay);
-  console.log(position);
-  for (i = 0; i <= position; i++) {
-    createPromise(i, delayPromise);
+  for (let item = 1; item <= position; item++) {
+    createPromise(item, delayPromise)
+      .then(result => {
+        console.log(`✅`, result);
+        Notiflix.Notify.success(result);
+      })
+      .catch(error => {
+        console.log(`❌`, error);
+        Notiflix.Notify.failure(error);
+      });
     delayPromise += Number(delay);
   }
 }
 
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  } else {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve(`Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        reject(`Rejected promise ${position} in ${delay}ms`);
+      }
+    }, delay);
+  });
 }
